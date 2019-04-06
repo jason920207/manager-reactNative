9da3674ab6f04b26eb70506bbd2e7c72d2a01401
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-04-06 16:10:05 -0400

    add get list from firebase

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 import { Picker, Text } from 'react-native'
 import { Card, CardSection, Input, Button } from './common'
 import { connect } from 'react-redux'
-import { employeeUpdate } from '../actions'
+import { employeeUpdate, employeeCreate } from '../actions'
 class EmployeeCreate extends Component {
+  onButtonPress(){
+    const { name, phone, shift } = this.props
+
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' })
+  }
   render(){
     return (
       <Card>
@@ -43,7 +48,7 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -64,4 +69,4 @@ const mapStateToprops = (state) => {
   return { name, phone, shift }
 }
 
-export default connect(mapStateToprops, { employeeUpdate } )(EmployeeCreate)
+export default connect(mapStateToprops, { employeeUpdate, employeeCreate } )(EmployeeCreate)
